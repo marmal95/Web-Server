7f4b04280e9f49eb3e88450843310a8c8c49f208
marmal95 <dev.marmal195@gmail.com>
2018-03-03 17:10:44 +0000

    Remove old include / source

--- include/Header.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct Header
-{
-    std::string name;
-    std::string value;
-};
--- source/Exception.cpp
+++
@@ -1,9 +0,0 @@
-#include "Exception.hpp"
-
-FileNotFound::FileNotFound(std::string_view message)
-	: std::runtime_error{ message.data() }
-{}
-
-RequestInvalid::RequestInvalid(std::string_view message)
-	: std::runtime_error{ message.data() }
-{}
